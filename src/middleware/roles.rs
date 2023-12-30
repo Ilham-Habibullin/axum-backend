71921b1a2c5bc91ccaa519e792f901e00050d78a
ilkham khabibullin <mailbox.ilham@gmail.com>
2023-12-30 15:25:10 +0300

    handle absence of user in roles middleware

--- src/middleware/roles.rs
+++ src/middleware/roles.rs
@@ -8,7 +8,9 @@ use axum::{
 use crate::{Roles, modules::users::types::User};
 
 pub async fn roles(State(role): State<Roles>, req: Request, next: Next) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let x = req.extensions().get::<User>().unwrap();
+    let x = req.extensions().get::<User>().ok_or_else(|| {
+        (StatusCode::FORBIDDEN, "Not enough rights".to_string())
+    })?;
 
     if (x.role.clone() as i16) < (role as i16) {
         Err((StatusCode::FORBIDDEN, "Not enough rights".to_string()))
